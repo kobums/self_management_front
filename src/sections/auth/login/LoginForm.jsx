a847816a752a4af65d81a150cad9e8379dbe9a17
고범석 <kobums23@gmail.com>
2023-04-10 16:51:01 +0900

    minimal-ui-kit/material-kit-react style 젹용 및 login page publishing

---
+++ src/sections/auth/login/LoginForm.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// @mui
+import {
+  Link,
+  Stack,
+  IconButton,
+  InputAdornment,
+  TextField,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  Checkbox,
+} from "@mui/material";
+
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
+import { LoadingButton } from "@mui/lab";
+// components
+import Iconify from "../../../components/iconify";
+
+export default function LoginForm() {
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClick = () => {
+    navigate("/dashboard", { replace: true });
+  };
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <Stack spacing={3}>
+        <TextField name="email" label="Email address" />
+
+        <FormControl sx={{ m: 1 }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+      </Stack>
+
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+        sx={{ my: 2 }}
+      >
+        <Checkbox name="remember" label="Remember me" />
+        <Link variant="subtitle2" underline="hover">
+          Forgot password?
+        </Link>
+      </Stack>
+
+      <LoadingButton
+        fullWidth
+        size="large"
+        type="submit"
+        variant="contained"
+        onClick={handleClick}
+      >
+        Login
+      </LoadingButton>
+    </>
+  );
+}
